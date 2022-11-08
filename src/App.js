6ec0aa3c35f09d48b37b7932a69ef9d43f4b0da2
Sayan De <sayandeten@gmail.com>
2022-11-09 00:06:47 +0530

    Add Create React App Files(Initial Commit)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Home from "./components/home";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Education from "./components/education";
+import Projects from "./components/projects";
+import Contactme from "./components/contactme";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home />
+      <About />
+      <Skills />
+      <Education />
+      <Projects />
+      <Contactme />
+    </>
   );
 }
 
---
+++ src/components/contactme.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contactme() {
+  return (
+    <>
+      <h1>Contactme</h1>
+    </>
+  );
+}
